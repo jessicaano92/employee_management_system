730779fa1653b0b2c3bba2508105e5e477f7abcf
Jessica Anocibar <jessicayaws@yahoo.com>
2020-10-26 14:36:10 -0600

    added files

---
+++ index.js
@@ -0,0 +1,9 @@
+//pseudocode
+
+const inquirer = require("inquirer");
+//will need inquirer.prompt to prompt the user for questions regarding employees
+
+
+
+
+//will need id, firstName, lastName, title, department, salary, manager
