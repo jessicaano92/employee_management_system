d59d89de867c182aca8df53001d21847d27498be
Jessica Anocibar <jessicayaws@yahoo.com>
2020-10-27 10:23:25 -0600

    modified sql and added json

--- employeeSystem.sql
+++ employeeSystem.sql
@@ -17,32 +17,36 @@
   * **last_name** - VARCHAR(30) to hold employee last name
   * **role_id** - INT to hold reference to role employee has
   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager*/
-DROP DATABASE IF EXISTS employee_system_db;
-CREATE DATABASE employee_system_db;
-USE employee_system_db;
+
+--possibly add an INDEX for large amount of data
+      --special table for SQL for look ups in a more efficient way
+
+
+DROP DATABASE IF EXISTS employeeSystem_db;
+CREATE DATABASE employeeSystem_db;
+USE employeeSystem_db;
 
 
 
   CREATE TABLE department (
-      id INT(10),
+      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
       name VARCHAR(30),
-      PRIMARY KEY (id)
   );
 
   CREATE TABLE role (
-      id INT (10),
+      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
       title VARCHAR(30), 
       salary --don't know decimal
-      department_id INT (10),
-      PRIMARY KEY (id)
+      department_id INT UNSIGNED,
+      
   );
 
   CREATE TABLE employee (
-      id INT (10),
+      id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
       first_name VARCHAR(30),
       last_name VARCHAR(30),
-      role_id INT (10),
-      manager_id INT(10) NULL
+      role_id INT UNSIGNED,
+      manager_id INT UNSIGNED NULL
   )
 
   
